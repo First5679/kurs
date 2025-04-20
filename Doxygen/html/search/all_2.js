c92158f709c65db9d0e9b58d3876a258d8d3e162
First5679 <mr.vania678@mail.ru>
2025-04-20 15:29:58 +0300

    Add files via upload

---
+++ Doxygen/html/search/all_2.js
@@ -0,0 +1,7 @@
+var searchData=
+[
+  ['e_5ferror_5',['e_error',['../classServer.html#a0761dd50aca6cfdd1ad2d165d08dbcf8',1,'Server::e_error()'],['../classAuthorized.html#a68d3b9443e59267617e5a598cf443ccd',1,'Authorized::e_error()'],['../classCalculator.html#a9df4334cb9b557ae42f5e098fc1ee6e1',1,'Calculator::e_error()']]],
+  ['er_6',['er',['../classError.html#a6dd10cbba36e50ee58c16e67e22b1a55',1,'Error']]],
+  ['error_7',['Error',['../classError.html',1,'Error'],['../classError.html#aca339d00ad8481fb4c184f0ece42698b',1,'Error::Error()']]],
+  ['errors_8',['errors',['../classError.html#a1129e50b258f1d1b4513f0b7736d9b4e',1,'Error']]]
+];
---
+++ Doxygen/html/search/functions_4.js
@@ -0,0 +1,6 @@
+var searchData=
+[
+  ['salt_5fgenerator_30',['salt_generator',['../classAuthorized.html#a3f29c5f770b91d76121e7bec0848f33a',1,'Authorized']]],
+  ['self_5faddr_31',['self_addr',['../classServer.html#aeb0aec2fefebfdb6583e3df93d9672cb',1,'Server']]],
+  ['server_32',['Server',['../classServer.html#af2d7ac300139aaab894149fdb155e3ec',1,'Server']]]
+];
